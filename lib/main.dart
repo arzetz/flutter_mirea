4e5a4a689daab7d147b2deecf40737437f3d319e
Johnny Silverhand <73528373+arzetz@users.noreply.github.com>
2024-09-23 04:09:03 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,132 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyForm());
+  runApp(MaterialApp(home: MyWidget()));
 }
 
-class MyForm extends StatelessWidget {
-  const MyForm({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Форма',
-      home: const MyWidget(),
-    );
-  }
-}
-
-class MyWidget extends StatelessWidget {
+class MyWidget extends StatefulWidget {
   const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  bool _isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Column(
-        children: <Widget>[
-          // Текст "Авторизация" вверху
+        children: [
           Padding(
-            padding: const EdgeInsets.only(top: 152.0), // Отступ сверху
+            padding: const EdgeInsets.only(top: 132.0),
             child: Center(
               child: Text(
                 "Авторизация",
-                style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
-                ),
+                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
               ),
             ),
           ),
-
-          // Отступ для разделения
           SizedBox(height: 22),
-
-          // Форма в центре
           Padding(
-            padding: const EdgeInsets.only(top: 152.0),
-          ), // Отступ сверху
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Center(
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Логин',
-                  border: OutlineInputBorder(),
-                  fillColor: Colors.black12,
-                  filled: true,
+            padding: const EdgeInsets.fromLTRB(16.0, 70.0, 16.0, 0.0),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Логин',
+                      border: InputBorder.none,
+                      fillColor: const Color.fromARGB(255, 243, 243, 243),
+                      filled: true,
+                    ),
+                  ),
                 ),
-              ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: 'Пароль',
+                        border: InputBorder.none,
+                        fillColor: const Color.fromARGB(255, 235, 235, 235),
+                        filled: true),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Checkbox(
+                      value: _isChecked,
+                      onChanged: (value) {
+                        setState(() {
+                          _isChecked = value!;
+                        });
+                      },
+                    ),
+                    Text(
+                      'Запомнить меня',
+                      style: TextStyle(fontSize: 16),
+                    ), // Текст справа от чекбокса
+                  ],
+                ),
+                SizedBox(height: 10),
+                Container(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF2771FF),
+                      padding: EdgeInsets.all(10.0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        side: const BorderSide(color: Color(0xFF00A0E3)),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      "Войти",
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16),
+                Container(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        side: const BorderSide(color: Color(0xFF00A0E3)),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      "Регистрация",
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: const Color(0xFF2E7FA8),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16),
+                Text(
+                  'Восстановить пароль',
+                  style: TextStyle(fontSize: 15),
+                )
+              ],
             ),
           ),
-          CheckboxListTile(
-            title: Text("title text"),
-            value: null,
-            onChanged: (newValue) {},
-            controlAffinity:
-                ListTileControlAffinity.leading, //  <-- leading Checkbox
-          ),
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: InputDecoration(
-                  hintText: 'Пароль',
-                  border: OutlineInputBorder(),
-                  fillColor: Colors.black12,
-                  filled: true),
-            ),
-          ),
-          ElevatedButton(onPressed: () {}, child: Text('Войти')),
-          ElevatedButton(onPressed: () {}, child: Text('Регистрация')),
         ],
       ),
     );
   }
 }
+
